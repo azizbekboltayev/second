d2d3001fff5aff0d787f4b4163dee4631b475321
Azizbek <azizbekboltaev25@gmail.com>
2024-02-24 16:57:53 +0500

    changes in mpl.py' '

--- mpl.py
+++ mpl.py
@@ -11,6 +11,6 @@ ax.plot(input_values, squares, linewidth=3)
 
 ax.set_title("Square numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
-ax.set_ylabel("Square of Value", fontsize = 14)
+ax.set_ylabel("Square of Value",fontsize = 14)
 ax.tick_params(labelsize=14)
 plt.show()
